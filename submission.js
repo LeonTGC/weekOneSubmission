283b5c73a1e8aa6b0602fa3bb14d6bbefe64e398
Cooper <leon.cooper@gceducationandskills.ac.uk>
2022-10-17 09:53:03 +0100

    first commit

---
+++ submission.js
@@ -0,0 +1,22 @@
+// challenge 1
+
+console.log(Math.round(Math.random() * 10))
+
+
+// challenge 2
+
+let firstName = 'dave'
+
+console.log(`my name is ${firstName}`)
+
+// challenge 3
+
+
+let num = Math.floor(Math.random() * 10)
+while (num != 5){
+    console.log(num + " thats not 5")
+    num = Math.floor(Math.random() * 10)
+}
+console.log("its 5!!!")
+// challenge 4
+// challenge 5
